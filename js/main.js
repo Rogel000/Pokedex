118a55da306aff3688be7d46714adc2521b49cc5
Rogel000 <roge.lucie.dev@gmail.com>
2024-08-29 09:14:12 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const baseUrl = "https://pokeapi.co/api/v2/";
-  const maxPokemonId = 1025;
+const baseUrl = "https://pokeapi.co/api/v2/";
+const maxPokemonId = 1025;
 
-  const btnSearch = document.getElementById("search");
-  const btnPrevious = document.getElementById("previous");
-  const btnNext = document.getElementById("next");
-  const inputField = document.getElementById("pokemonNameOrId");
-  const pokemonInfoDiv = document.getElementById("pokemonInfo");
-  const myPokemonsDiv = document.getElementById("myPokemons");
-  const modal = document.getElementById("myPokemonsModal");
-  const closeModalBtn = document.getElementById("closeModal");
-  const viewMyPokemonsBtn = document.getElementById("viewMyPokemons");
+const btnSearch = document.getElementById("search");
+const btnPrevious = document.getElementById("previous");
+const btnNext = document.getElementById("next");
+const inputField = document.getElementById("pokemonNameOrId");
+const pokemonInfoDiv = document.getElementById("pokemonInfo");
+const myPokemonsDiv = document.getElementById("myPokemons");
+const modal = document.getElementById("myPokemonsModal");
+const closeModalBtn = document.getElementById("closeModal");
+const viewMyPokemonsBtn = document.getElementById("viewMyPokemons");
 
-  let myPokemons = JSON.parse(localStorage.getItem("myPokemons")) || []; 
+let myPokemons = JSON.parse(localStorage.getItem("myPokemons")) || [];
 
-  async function fetchAndDisplayPokemon(query) {
-    try {
-      const response = await fetch(`${baseUrl}pokemon/${query}`);
-      if (!response.ok) {
-        throw new Error("Pokémon non trouvé");
-      }
-      const data = await response.json();
-      displayPokemon(data);
-    } catch (error) {
-      pokemonInfoDiv.innerHTML = `<p>${error.message}</p>`;
+async function fetchAndDisplayPokemon(query) {
+  try {
+    const response = await fetch(`${baseUrl}pokemon/${query}`);
+    if (!response.ok) {
+      throw new Error("Pokémon non trouvé");
     }
+    const data = await response.json();
+    displayPokemon(data);
+  } catch (error) {
+    pokemonInfoDiv.innerHTML = `<p>${error.message}</p>`;
   }
+}
 
-  function displayPokemon(pokemonData) {
-    const { name, id, weight, height, types, abilities, sprites } = pokemonData;
-    const pokemonInfoContainer = document.querySelector(
-      ".pokemon-info-container"
-    );
-    const navBtn = document.querySelector(".nav-buttons");
-    pokemonInfoContainer.style.display = "flex";
-    navBtn.style.display = "flex";
+function displayPokemon(pokemonData) {
+  const { name, id, weight, height, types, abilities, sprites } = pokemonData;
+  const pokemonInfoContainer = document.querySelector(
+    ".pokemon-info-container"
+  );
+  const navBtn = document.querySelector(".nav-buttons");
+  pokemonInfoContainer.style.display = "flex";
+  navBtn.style.display = "flex";
 
-    pokemonInfoDiv.dataset.id = id;
-    pokemonInfoDiv.innerHTML = `
+  pokemonInfoDiv.dataset.id = id;
+  pokemonInfoDiv.innerHTML = `
           <img src="pokeball.png" class="pokeball" />  
           <img src="${
             sprites.other["official-artwork"].front_default
@@ -63,88 +62,87 @@ document.addEventListener("DOMContentLoaded", () => {
           </div>
         `;
 
-    const capturePokemon = pokemonInfoDiv.querySelector(".pokeball");
-    capturePokemon.addEventListener("click", () => {
-      const pokemonId = parseInt(pokemonInfoDiv.dataset.id);
+  const capturePokemon = pokemonInfoDiv.querySelector(".pokeball");
+  capturePokemon.addEventListener("click", () => {
+    const pokemonId = parseInt(pokemonInfoDiv.dataset.id);
 
-      if (myPokemons.includes(pokemonId)) {
-        alert("Vous avez déjà capturé ce Pokémon!");
-      } else {
-        myPokemons.push(pokemonId);
-        localStorage.setItem("myPokemons", JSON.stringify(myPokemons));
-        alert("Pokémon capturé!");
-      }
-    });
-  }
+    if (myPokemons.includes(pokemonId)) {
+      alert("Vous avez déjà capturé ce Pokémon!");
+    } else {
+      myPokemons.push(pokemonId);
+      localStorage.setItem("myPokemons", JSON.stringify(myPokemons));
+      alert("Pokémon capturé!");
+    }
+  });
+}
 
-  function displayMyPokemons() {
-    const myPokemons = JSON.parse(localStorage.getItem("myPokemons")) || [];
-    myPokemonsDiv.innerHTML = "";
+function displayMyPokemons() {
+  const myPokemons = JSON.parse(localStorage.getItem("myPokemons")) || [];
+  myPokemonsDiv.innerHTML = "";
 
-    myPokemons.forEach((pokemonId) => {
-      fetch(`${baseUrl}pokemon/${pokemonId}`)
-        .then((response) => response.json())
-        .then((pokemonData) => {
-          const { id, name, sprites } = pokemonData;
-          const pokemonCard = document.createElement("div");
-          pokemonCard.classList.add("pokemon-card");
-          pokemonCard.innerHTML = `
+  myPokemons.forEach((pokemonId) => {
+    fetch(`${baseUrl}pokemon/${pokemonId}`)
+      .then((response) => response.json())
+      .then((pokemonData) => {
+        const { id, name, sprites } = pokemonData;
+        const pokemonCard = document.createElement("div");
+        pokemonCard.classList.add("pokemon-card");
+        pokemonCard.innerHTML = `
             <img src="${sprites.other["official-artwork"].front_default}" alt="${name}" class="pokemon-image">
             <h2>${id} - ${name}</h2>
        
           `;
 
-          myPokemonsDiv.appendChild(pokemonCard);
-        });
-    });
-    modal.style.display = "flex";
+        myPokemonsDiv.appendChild(pokemonCard);
+      });
+  });
+  modal.style.display = "flex";
+}
+
+function openMyPokemonsModal() {
+  displayMyPokemons();
+}
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+viewMyPokemonsBtn.addEventListener("click", openMyPokemonsModal);
+
+btnSearch.addEventListener("click", () => {
+  const query = inputField.value.trim();
+  if (query) {
+    fetchAndDisplayPokemon(query);
+    inputField.value = "";
+  } else {
+    alert("Veuillez entrer un nom ou un numéro de Pokédex.");
   }
+});
 
-  function openMyPokemonsModal() {
-    displayMyPokemons();
+inputField.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    btnSearch.click();
   }
+});
 
-  closeModalBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
+btnPrevious.addEventListener("click", () => {
+  const currentId = parseInt(pokemonInfoDiv.dataset.id);
+  if (currentId && currentId > 1) {
+    fetchAndDisplayPokemon(currentId - 1);
+  } else if (currentId === 1) {
+    fetchAndDisplayPokemon(maxPokemonId);
+  } else {
+    alert("Veuillez d'abord rechercher un Pokémon.");
+  }
+});
 
-  viewMyPokemonsBtn.addEventListener("click", openMyPokemonsModal);
-
-  btnSearch.addEventListener("click", () => {
-    const query = inputField.value.trim();
-    if (query) {
-      fetchAndDisplayPokemon(query);
-      inputField.value = "";
-    } else {
-      alert("Veuillez entrer un nom ou un numéro de Pokédex.");
-    }
-  });
-
-  inputField.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      btnSearch.click();
-    }
-  });
-
-  btnPrevious.addEventListener("click", () => {
-    const currentId = parseInt(pokemonInfoDiv.dataset.id);
-    if (currentId && currentId > 1) {
-      fetchAndDisplayPokemon(currentId - 1);
-    } else if (currentId === 1) {
-      fetchAndDisplayPokemon(maxPokemonId);
-    } else {
-      alert("Veuillez d'abord rechercher un Pokémon.");
-    }
-  });
-
-  btnNext.addEventListener("click", () => {
-    const currentId = parseInt(pokemonInfoDiv.dataset.id);
-    if (currentId && currentId < maxPokemonId) {
-      fetchAndDisplayPokemon(currentId + 1);
-    } else if (currentId === maxPokemonId) {
-      fetchAndDisplayPokemon(1);
-    } else {
-      alert("Veuillez d'abord rechercher un Pokémon.");
-    }
-  });
+btnNext.addEventListener("click", () => {
+  const currentId = parseInt(pokemonInfoDiv.dataset.id);
+  if (currentId && currentId < maxPokemonId) {
+    fetchAndDisplayPokemon(currentId + 1);
+  } else if (currentId === maxPokemonId) {
+    fetchAndDisplayPokemon(1);
+  } else {
+    alert("Veuillez d'abord rechercher un Pokémon.");
+  }
 });
